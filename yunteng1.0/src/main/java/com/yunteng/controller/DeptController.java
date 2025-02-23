b9d4d7fa03c16edbe95dddfbd76c1c2e380590d3
lijunyii <2336287274@qq.com>
2025-02-23 17:46:01 +0800

    增加了异常处理

--- yunteng1.0/src/main/java/com/yunteng/controller/DeptController.java
+++ yunteng1.0/src/main/java/com/yunteng/controller/DeptController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.yunteng.service.DeptService;
 
-
 /**
  * 部门管理Controller
  */
@@ -21,39 +20,54 @@ public class DeptController {
 
     /**
      * 查询全部部门数据
-     * @return
+     * @return 包含部门列表的结果对象
      */
     @GetMapping("/depts")
-    public Result list(){
+    public Result list() {
         log.info("查询全部部门数据");
-        //调用service查询数据库
-        List<Dept> deptList=deptService.list();
-        return Result.success(deptList);
+        try {
+            // 调用service查询数据库
+            List<Dept> deptList = deptService.list();
+            return Result.success(deptList);
+        } catch (Exception e) {
+            log.error("查询部门数据时发生异常: ", e);
+            return Result.error("查询部门数据失败，请稍后重试");
+        }
     }
 
     /**
      * 删除部门
-     * @return
+     * @param id 部门ID
+     * @return 操作结果对象
      */
     @DeleteMapping("/depts/{id}")
-    public Result delete(@PathVariable Integer id){
-        log.info("根据id删除部门:{}",id);
-        //调用service删除部门
-        deptService.delete(id);
-        return Result.success();
+    public Result delete(@PathVariable Integer id) {
+        log.info("根据id删除部门:{}", id);
+        try {
+            // 调用service删除部门
+            deptService.delete(id);
+            return Result.success();
+        } catch (Exception e) {
+            log.error("删除部门时发生异常: ", e);
+            return Result.error("删除部门失败，请稍后重试");
+        }
     }
 
     /**
      * 添加部门
-     * @param dept
-     * @return
+     * @param dept 部门对象
+     * @return 操作结果对象
      */
     @PostMapping("/depts")
-    public Result add(@RequestBody Dept dept){
-        log.info("添加部门:{}",dept);
-        //调用service添加部门
-        deptService.add(dept);
-        return Result.success();
+    public Result add(@RequestBody Dept dept) {
+        log.info("添加部门:{}", dept);
+        try {
+            // 调用service添加部门
+            deptService.add(dept);
+            return Result.success();
+        } catch (Exception e) {
+            log.error("添加部门时发生异常: ", e);
+            return Result.error("添加部门失败，请稍后重试");
+        }
     }
-
 }
