90222383fcea33eeb6cdc5ed9b0ffe68ae377eee
lijunyii <2336287274@qq.com>
2025-03-08 16:54:20 +0800

    Revert "Merge branch 'branch2'"
    This reverts commit 069e0113cdfeca4545ac5de05f8c97b787e57a3d, reversing
    changes made to 998cde59ba0cd1e601ac5d44738f705398617c3e.

--- yunteng1.0/src/main/java/com/yunteng/service/ManageService.java
+++ yunteng1.0/src/main/java/com/yunteng/service/ManageService.java
@@ -6,11 +6,8 @@ import com.yunteng.pojo.Manage;
 
 import java.util.List;
 
-import com.yunteng.pojo.Manage;
-import java.util.List;
-
 /**
- * 员工管理
+ * 管理员管理
  */
 public interface ManageService {
     /**
@@ -31,37 +28,4 @@ public interface ManageService {
      * @param manage
      */
 void add(Manage manage);
-}
-
-
-    /*PageBean page(Integer page, Integer pageSize, String name, Short gender, LocalDate begin, LocalDate end);
-     */
-
-    /**
-     * 新增员工
-     *
-     * @param manage
-     */
-
-    void save(Manage manage);
-
-    /**
-     * 根据ID查询员工
-     *
-     * @param id
-     * @return
-     */
-    Manage getById(Integer id);
-
-
-    Manage login(Manage manage);
-
-    /**
-     * 管理员登录
-     *
-     * @param username 用户名
-     * @param password 密码
-     * @return 登录成功返回对应的Manage对象，失败返回null
-     */
-    Manage login(String username, String password);
 }
