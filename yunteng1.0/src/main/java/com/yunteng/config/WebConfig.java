21ba7a7cc371614e5658f1afb4b440e2e88db9a8
lijunyii <2336287274@qq.com>
2025-03-16 13:09:36 +0800

    前后端对接成功

--- yunteng1.0/src/main/java/com/yunteng/config/WebConfig.java
+++ yunteng1.0/src/main/java/com/yunteng/config/WebConfig.java
@@ -2,12 +2,14 @@ package com.yunteng.config;
 
 import com.yunteng.interceptor.LoginCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//配置类
 @Configuration
+@ComponentScan(basePackages = "com.yunteng")
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -16,5 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
